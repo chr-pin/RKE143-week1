32647c76c0dd32ca3686643269e65d0b181c7659
Chr P <christian.pintson@tktk.ee>
2023-10-01 12:28:24 +0300

    first

---
+++ server.js
@@ -0,0 +1,12 @@
+//console.log("Hello!");
+let firstName = 'Harry';
+let lastName = 'Potter';
+
+console.log("Hello, " + firstName + "!");
+
+//template literals
+
+console.log(`Hellio, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}!`
+console.log(greeting);
